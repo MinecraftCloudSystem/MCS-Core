b90cb0e1cddd949cb754b26ee12050ed244761a5
ilouHD <marctroll99@gmail.com>
2016-08-05 02:41:16 +0200

    added dashboard and daemon design

--- web/javascript/controller/DashboardCtrl.js
+++ web/javascript/controller/DashboardCtrl.js
@@ -19,4 +19,73 @@ app.controller('DashboardCtrl', ['$rootScope', '$scope', 'Socket', function($roo
 
     });
 
+    var playersOnline = [];
+    var last = 1950;
+    var max = 2000;
+    for (var i = 0; i < 30; i += 1) {
+        playersOnline.push([i, last]);
+        last += Math.floor((Math.random() * 4));
+    }
+    for (var k = 30; k < 60; k += 1) {
+        playersOnline.push([k, last]);
+        last -= Math.floor((Math.random() * 4));
+    }
+    var line_data1 = {
+        data: playersOnline,
+        color: "#3c8dbc"
+    };
+    $.plot("#line-chart", [line_data1], {
+        grid: {
+            hoverable: true,
+            borderColor: "#f3f3f3",
+            borderWidth: 1,
+            tickColor: "#f3f3f3"
+        },
+        series: {
+            shadowSize: 1,
+            lines: {
+                show: true
+            },
+            points: {
+                show: false
+            }
+        },
+        lines: {
+            fill: false,
+            color: ["#3c8dbc", "#f56954"]
+        },
+        yaxis: {
+            show: true,
+        },
+        xaxis: {
+            show: false
+        }
+    });
+    //Initialize tooltip on hover
+    $('<div class="tooltip-inner" id="line-chart-tooltip"></div>').css({
+        position: "absolute",
+        display: "none",
+        opacity: 0.8
+    }).appendTo("body");
+    $("#line-chart").bind("plothover", function (event, pos, item) {
+
+        if (item) {
+            var x = item.datapoint[0].toFixed(0),
+                y = item.datapoint[1].toFixed(0);
+
+            $("#line-chart-tooltip").html(y + " / " + max + " Players online <br/>" + calcTime(x))
+                .css({top: item.pageY + 5, left: item.pageX + 5})
+                .fadeIn(200);
+        } else {
+            $("#line-chart-tooltip").hide();
+        }
+
+    });
+
+    var calcTime = function(time) {
+        var minutes = parseInt(60 - time);
+
+        return minutes + " minute" + (minutes != 1 ? "s" : "") + " ago";
+    };
+
 }]);

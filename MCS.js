145a03e6304b4f6737054231a353716944ef1527
ilouHD <marctroll99@gmail.com>
2016-08-05 19:04:53 +0200

    added basic design for all pages

--- MCS.js
+++ MCS.js
@@ -5,8 +5,11 @@ var fs = require('fs');
 var MCSCore = require('./system/MCSCore.class');
 var mcs = new MCSCore();
 
+var JSZip = require("jszip");
+var YAML = require('yamljs');
+
 /**
- * Gets the SpaceBoard Core-Module
+ * Gets the MCS Core-Module
  * @returns {*}
  * */
 module.exports.getCore = function() {
@@ -33,4 +36,19 @@ process.on("SIGINT", () => {
     getCore().getLog().info("------------------------------");
     getCore().getLog().info("System shutting down...");
     process.exit(0);
+});
+
+fs.readFile("./files/plugins/autorestart.jar", function(err, data) {
+    if (err) {
+        throw err;
+    }
+    JSZip.loadAsync(data).then(function (zip) {
+        zip.file('plugin.yml').async("string").then(function(data) {
+            var info = YAML.parse(data);
+
+            /* getCore().getLog().debug("Name: " + info.name);
+             getCore().getLog().debug("Version: " + info.version);
+             getCore().getLog().debug("Author: " + (info.author ? info.author : info.authors));*/
+        });
+    });
 });

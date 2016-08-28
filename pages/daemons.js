796ffcf3b88ed4b93151c09f53187734dc6c3682
ilouHD <marctroll99@gmail.com>
2016-08-28 19:32:56 +0200

    added default add-request of different types

--- pages/daemons.js
+++ pages/daemons.js
@@ -39,7 +39,7 @@ exports.loadCache = function() {
     }
 };
 
-exports.addDaemon = function(name, ip, port) {
+exports.addDaemon = function(name, ip, port, callback) {
     if(MCS.getCore().getMongo().hasConnection()) {
         var Daemon = MCS.getCore().getMongo().getLibrary().connection.model('daemons');
 
@@ -51,14 +51,17 @@ exports.addDaemon = function(name, ip, port) {
         }).save((err) => {
             if(err) {
                 MCS.getCore().getLog().error("Unknown Error saving Daemon '" + name + "' to Database.");
+                callback("error-saving");
                 return;
             }
 
-            MCS.getCore().getLog().info("Daemon '" + name + "' daved to Database successfully.");
+            MCS.getCore().getLog().info("Daemon '" + name + "' saved to Database successfully.");
+            callback("success-saving");
 
             this.loadCache();
         });
     } else {
         MCS.getCore().getLog().warn("No Database-Connection ready. Is the database offline?");
+        callback("database-offline");
     }
 };
--- socket/SocketServer.js
+++ socket/SocketServer.js
@@ -8,11 +8,53 @@ var io = require('socket.io')(MCS.getCore().getWebServer().getWebserver());
 
 io.on('connection', function(socket) {
     socket.on("page-req", function(data) {
+        if(data == undefined) { return; }
         if(data.page == undefined) {
             MCS.getCore().getLog().error("Can't handle Page-Request. No page set!");
             socket.emit("page-res", { state: "error", message: "Can't handle Page-Request. No page set!" });
             return;
         }
         socket.emit("page-res", { state: "success", content: MCS.getCore().getPageManager().getPage(data.page) });
+    });
+
+    socket.on("add-req", function(data) {
+        if(data == undefined) { return; }
+        if(data.type == undefined) {
+            if(data.page == undefined) {
+                MCS.getCore().getLog().error("Can't handle Add-Request. No Type set!");
+                socket.emit("add-res", { state: "error", message: "Can't handle Add-Request. No Type set!" });
+                return;
+            }
+        }
+
+        switch(data.type) {
+            case "daemon":
+                var Daemon = require('../pages/daemons');
+                Daemon.addDaemon(data.name, data.ip, data.port, (status) => {
+                    switch(status) {
+                        case "database-offline":
+                            socket.emit("add-res", { state: "error", message: "Can't handle Add-Request. Database unavailable!" });
+                            break;
+                        case "error-saving":
+                            socket.emit("add-res", { state: "warn", message: "An unknown error occurred saving your Daemon!" });
+                            break;
+                        case "success-saving":
+                            socket.emit("add-res", { state: "success", message: "Saved your Daemon successfully!" });
+                            break;
+                        default:
+                            socket.emit("add-res", { state: "error", message: "An unknown error occurred!" });
+                    }
+                });
+                break;
+            case "plugin":
+                break;
+            case "world":
+                break;
+            case "servertype":
+                break;
+            default:
+                MCS.getCore().getLog().warn("Can't handle Add-Request. Unknown Type set!");
+                socket.emit("page-res", { state: "warn", message: "Can't handle Add-Request. Unknown Type set!" });
+        }
     });
 });
